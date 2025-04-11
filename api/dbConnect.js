3a1d72bdd30335be10551c125fe5fa49bb6919e1
Rashidsidhik <117706851+Rashidsidhik@users.noreply.github.com>
2025-04-11 19:51:47 +0530

    hfj

--- api/dbConnect.js
+++ api/dbConnect.js
@@ -1,24 +1,12 @@
-import { MongoClient } from "mongodb";
+import mongoose from 'mongoose';
 
-const uri = process.env.MONGODB_URI;
-const options = {};
-
-let client;
-let clientPromise;
-
-if (!process.env.MONGODB_URI) {
-    throw new Error("Please add your MongoDB URI");
-}
-
-if (process.env.NODE_ENV === "development") {
-    if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options);
-        global._mongoClientPromise = client.connect();
+const dbConnect = async () => {
+    if (mongoose.connections[0].readyState) {
+        console.log("Already Connected");
+        return;
     }
-    clientPromise = global._mongoClientPromise;
-} else {
-    client = new MongoClient(uri, options);
-    clientPromise = client.connect();
-}
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB Connected");
+};
 
-export default clientPromise;
+export default dbConnect;
